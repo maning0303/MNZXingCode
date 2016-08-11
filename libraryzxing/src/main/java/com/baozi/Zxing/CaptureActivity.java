e3b04cf2a416e75f2e1b657165b8d57d6a6fa6f1
maning <ning.ma@12301.cn>
2016-08-11 22:32:40 +0800

    添加权限，修复可能图库选择图片无法获取路径问题

--- libraryzxing/src/main/java/com/baozi/Zxing/CaptureActivity.java
+++ libraryzxing/src/main/java/com/baozi/Zxing/CaptureActivity.java
@@ -5,18 +5,15 @@ import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
 import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Looper;
 import android.os.Vibrator;
 import android.provider.MediaStore;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceHolder.Callback;
 import android.view.SurfaceView;
@@ -30,36 +27,22 @@ import android.widget.Toast;
 import com.baozi.Zxing.camera.CameraManager;
 import com.baozi.Zxing.decoding.CaptureActivityHandler;
 import com.baozi.Zxing.decoding.InactivityTimer;
-import com.baozi.Zxing.decoding.RGBLuminanceSource;
-import com.baozi.Zxing.decoding.Utils;
+import com.baozi.Zxing.utils.ZXingUtils;
 import com.baozi.Zxing.view.ViewfinderView;
 import com.google.zxing.BarcodeFormat;
-import com.google.zxing.BinaryBitmap;
-import com.google.zxing.ChecksumException;
-import com.google.zxing.DecodeHintType;
-import com.google.zxing.FormatException;
-import com.google.zxing.LuminanceSource;
-import com.google.zxing.MultiFormatReader;
-import com.google.zxing.NotFoundException;
-import com.google.zxing.PlanarYUVLuminanceSource;
 import com.google.zxing.Result;
-import com.google.zxing.common.HybridBinarizer;
-import com.google.zxing.qrcode.QRCodeReader;
 import com.maning.libraryzxing.R;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
-import java.util.Hashtable;
 import java.util.Vector;
 
 
 /**
  * 拍照的Activity
- *
- * @author Baozi
  */
 public class CaptureActivity extends Activity implements Callback, OnClickListener {
+
+    private static final String TAG = CaptureActivity.class.getSimpleName();
 
     private CaptureActivityHandler handler;
     private ViewfinderView viewfinderView;
@@ -72,13 +55,12 @@ public class CaptureActivity extends Activity implements Callback, OnClickListen
     private static final float BEEP_VOLUME = 0.10f;
     private boolean vibrate;
     private String photo_path;
-    private Bitmap scanBitmap;
     private ImageView scanLine;
     private TranslateAnimation translateAnimation;
     private ImageView mo_scanner_back;
     private ImageView mo_scanner_photo;
-    private ImageView mo_scanner_light;
     private ImageView mo_scanner_histroy;
+    private ImageView mo_scanner_light;
     private boolean isShowHistory;
 
     /**
@@ -99,6 +81,13 @@ public class CaptureActivity extends Activity implements Callback, OnClickListen
         hasSurface = false;
         inactivityTimer = new InactivityTimer(this);
 
+        //初始化动画
+        initAnimation();
+
+
+    }
+
+    private void initAnimation() {
         //扫描动画
         translateAnimation = new TranslateAnimation(Animation.RELATIVE_TO_PARENT, 0.0f, Animation
                 .RELATIVE_TO_PARENT, 0.0f, Animation.RELATIVE_TO_PARENT, 0.0f, Animation.RELATIVE_TO_PARENT,
@@ -107,7 +96,6 @@ public class CaptureActivity extends Activity implements Callback, OnClickListen
         translateAnimation.setRepeatCount(-1);
         translateAnimation.setRepeatMode(Animation.RESTART);
         scanLine.startAnimation(translateAnimation);
-
     }
 
     private void initIntent() {
@@ -142,7 +130,7 @@ public class CaptureActivity extends Activity implements Callback, OnClickListen
     boolean flag = true;
 
     protected void light() {
-        if (flag == true) {
+        if (flag) {
             flag = false;
             // 开闪光灯
             CameraManager.get().openLight();
@@ -153,166 +141,75 @@ public class CaptureActivity extends Activity implements Callback, OnClickListen
             CameraManager.get().offLight();
             mo_scanner_light.setBackgroundResource(R.drawable.zxing_circle_trans_black);
         }
-        mo_scanner_light.setPadding(dip2px(8), dip2px(8), dip2px(8), dip2px(8));
+        mo_scanner_light.setPadding(
+                ZXingUtils.dip2px(this, 8),
+                ZXingUtils.dip2px(this, 8),
+                ZXingUtils.dip2px(this, 8),
+                ZXingUtils.dip2px(this, 8)
+        );
 
-    }
-
-    /**
-     * 根据手机的分辨率�?dp 的单�?转成�?px(像素)
-     */
-    public int dip2px(float dpValue) {
-        final float scale = this.getResources().getDisplayMetrics().density;
-        return (int) (dpValue * scale + 0.5f);
     }
 
     private void photo() {
-
-        Intent innerIntent = new Intent(); // "android.intent.action.GET_CONTENT"
+        Intent innerIntent = new Intent();
         if (Build.VERSION.SDK_INT < 19) {
             innerIntent.setAction(Intent.ACTION_GET_CONTENT);
         } else {
             innerIntent.setAction(Intent.ACTION_OPEN_DOCUMENT);
         }
-        // innerIntent.setAction(Intent.ACTION_GET_CONTENT);
-
         innerIntent.setType("image/*");
-
         Intent wrapperIntent = Intent.createChooser(innerIntent, "选择二维码图片");
-
-        CaptureActivity.this
-                .startActivityForResult(wrapperIntent, ZXingConstants.ScanPhotosRequestCode);
+        startActivityForResult(wrapperIntent, ZXingConstants.ScanPhotosRequestCode);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
         super.onActivityResult(requestCode, resultCode, data);
-
         if (resultCode == RESULT_OK) {
-
             switch (requestCode) {
-
                 case ZXingConstants.ScanPhotosRequestCode:
-
+                    //获取图片路径
                     String[] proj = {MediaStore.Images.Media.DATA};
-                    // 获取选中图片的路径
-                    Cursor cursor = getContentResolver().query(data.getData(),
-                            proj, null, null, null);
-
-                    if (cursor.moveToFirst()) {
-
-                        int column_index = cursor
-                                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-                        photo_path = cursor.getString(column_index);
-                        if (photo_path == null) {
-                            photo_path = Utils.getPath(getApplicationContext(),
-                                    data.getData());
-                            Log.i("123path  Utils", photo_path);
+                    Cursor cursor = getContentResolver().query(data.getData(), proj, null, null, null);
+                    if (cursor != null) {
+                        int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                        if (cursor.moveToFirst()) {
+                            photo_path = cursor.getString(columnIndex);
                         }
-                        Log.i("123path", photo_path);
-
+                        cursor.close();
                     }
-
-                    cursor.close();
-
-                    new Thread(new Runnable() {
-
-                        @Override
-                        public void run() {
-
-                            Result result = scanningImage(photo_path);
-                            // String result = decode(photo_path);
-                            if (result == null) {
-                                Log.i("123", "   -----------");
-                                Looper.prepare();
-                                Toast.makeText(getApplicationContext(), "图片格式有误", Toast.LENGTH_SHORT)
-                                        .show();
-                                Looper.loop();
-                            } else {
-                                Log.i("123result", result.toString());
-                                // Log.i("123result", result.getText());
-                                // 数据返回
-                                String recode = recode(result.toString());
-                                Intent data = new Intent();
-                                data.putExtra(ZXingConstants.ScanResult, recode);
-                                setResult(ZXingConstants.ScanRequestCode, data);
-                                finish();
-                            }
-                        }
-                    }).start();
+                    //解析图片
+                    analysisImage(photo_path);
                     break;
-
             }
-
         }
-
     }
 
-    // TODO: 解析部分图片
-    protected Result scanningImage(String path) {
-        if (TextUtils.isEmpty(path)) {
-
-            return null;
-
+    private void analysisImage(final String photoPath) {
+        if (TextUtils.isEmpty(photoPath)) {
+            Toast.makeText(this, "获取图片失败", Toast.LENGTH_SHORT).show();
+            return;
         }
-        // DecodeHintType 和EncodeHintType
-        Hashtable<DecodeHintType, String> hints = new Hashtable<DecodeHintType, String>();
-        hints.put(DecodeHintType.CHARACTER_SET, "utf-8"); // 设置二维码内容的编码
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true; // 先获取原大小
-        scanBitmap = BitmapFactory.decodeFile(path, options);
-        options.inJustDecodeBounds = false; // 获取新的大小
-
-        int sampleSize = (int) (options.outHeight / (float) 200);
-
-        if (sampleSize <= 0)
-            sampleSize = 1;
-        options.inSampleSize = sampleSize;
-        scanBitmap = BitmapFactory.decodeFile(path, options);
-
-        // --------------测试的解析方法---PlanarYUVLuminanceSource-这几行代码对project没作功----------
-
-        LuminanceSource source1 = new PlanarYUVLuminanceSource(
-                rgb2YUV(scanBitmap), scanBitmap.getWidth(),
-                scanBitmap.getHeight(), 0, 0, scanBitmap.getWidth(),
-                scanBitmap.getHeight(), false);
-        BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(
-                source1));
-        MultiFormatReader reader1 = new MultiFormatReader();
-        Result result1;
-        try {
-            result1 = reader1.decode(binaryBitmap);
-            String content = result1.getText();
-            Log.i("123content", content);
-        } catch (NotFoundException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
-
-        // ----------------------------
-
-        RGBLuminanceSource source = new RGBLuminanceSource(scanBitmap);
-        BinaryBitmap bitmap1 = new BinaryBitmap(new HybridBinarizer(source));
-        QRCodeReader reader = new QRCodeReader();
-        try {
-
-            return reader.decode(bitmap1, hints);
-
-        } catch (NotFoundException e) {
-
-            e.printStackTrace();
-
-        } catch (ChecksumException e) {
-
-            e.printStackTrace();
-
-        } catch (FormatException e) {
-
-            e.printStackTrace();
-
-        }
-
-        return null;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final String result = ZXingUtils.syncDecodeQRCode(photoPath);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (TextUtils.isEmpty(result)) {
+                            Toast.makeText(CaptureActivity.this, "图片格式错误", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // 数据返回
+                            Intent data = new Intent();
+                            data.putExtra(ZXingConstants.ScanResult, result);
+                            setResult(ZXingConstants.ScanRequestCode, data);
+                            finish();
+                        }
+                    }
+                });
+            }
+        }).start();
 
     }
 
@@ -362,9 +259,7 @@ public class CaptureActivity extends Activity implements Callback, OnClickListen
     private void initCamera(SurfaceHolder surfaceHolder) {
         try {
             CameraManager.get().openDriver(surfaceHolder);
-        } catch (IOException ioe) {
-            return;
-        } catch (RuntimeException e) {
+        } catch (Exception ioe) {
             return;
         }
         if (handler == null) {
@@ -410,7 +305,7 @@ public class CaptureActivity extends Activity implements Callback, OnClickListen
     public void handleDecode(final Result result, Bitmap barcode) {
         inactivityTimer.onActivity();
         playBeepSoundAndVibrate();
-        String recode = recode(result.toString());
+        String recode = ZXingUtils.recode(result.toString());
         // 数据返回
         Intent data = new Intent();
         data.putExtra(ZXingConstants.ScanResult, recode);
@@ -463,76 +358,6 @@ public class CaptureActivity extends Activity implements Callback, OnClickListen
         }
     };
 
-    /**
-     * 中文乱码
-     * <p/>
-     * 暂时解决大部分的中文乱码 但是还有部分的乱码无法解决 .
-     * <p/>
-     * 如果您有好的解决方式 请联系 2221673069@qq.com
-     * <p/>
-     * 我会很乐意向您请教 谢谢您
-     *
-     * @return
-     */
-    private String recode(String str) {
-        String formart = "";
-
-        try {
-            boolean ISO = Charset.forName("ISO-8859-1").newEncoder()
-                    .canEncode(str);
-            if (ISO) {
-                formart = new String(str.getBytes("ISO-8859-1"), "GB2312");
-                Log.i("1234      ISO8859-1", formart);
-            } else {
-                formart = str;
-                Log.i("1234      stringExtra", str);
-            }
-        } catch (UnsupportedEncodingException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return formart;
-    }
-
-    /**
-     * //TODO: TAOTAO 将bitmap由RGB转换为YUV //TOOD: 研究中
-     *
-     * @param bitmap 转换的图形
-     * @return YUV数据
-     */
-    public byte[] rgb2YUV(Bitmap bitmap) {
-        // 该方法来自QQ空间
-        int width = bitmap.getWidth();
-        int height = bitmap.getHeight();
-        int[] pixels = new int[width * height];
-        bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
-
-        int len = width * height;
-        byte[] yuv = new byte[len * 3 / 2];
-        int y, u, v;
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                int rgb = pixels[i * width + j] & 0x00FFFFFF;
-
-                int r = rgb & 0xFF;
-                int g = (rgb >> 8) & 0xFF;
-                int b = (rgb >> 16) & 0xFF;
-
-                y = ((66 * r + 129 * g + 25 * b + 128) >> 8) + 16;
-                u = ((-38 * r - 74 * g + 112 * b + 128) >> 8) + 128;
-                v = ((112 * r - 94 * g - 18 * b + 128) >> 8) + 128;
-
-                y = y < 16 ? 16 : (y > 255 ? 255 : y);
-                u = u < 0 ? 0 : (u > 255 ? 255 : u);
-                v = v < 0 ? 0 : (v > 255 ? 255 : v);
-
-                yuv[i * width + j] = (byte) y;
-                // yuv[len + (i >> 1) * width + (j & ~1) + 0] = (byte) u;
-                // yuv[len + (i >> 1) * width + (j & ~1) + 1] = (byte) v;
-            }
-        }
-        return yuv;
-    }
 
     @Override
     public void onClick(View v) {
--- libraryzxing/src/main/java/com/baozi/Zxing/utils/ZXingUtils.java
+++ libraryzxing/src/main/java/com/baozi/Zxing/utils/ZXingUtils.java
@@ -1,22 +1,70 @@
 package com.baozi.Zxing.utils;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.text.TextUtils;
+import android.util.Log;
 
+import com.baozi.Zxing.decoding.RGBLuminanceSource;
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.ChecksumException;
+import com.google.zxing.DecodeHintType;
 import com.google.zxing.EncodeHintType;
+import com.google.zxing.FormatException;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.PlanarYUVLuminanceSource;
+import com.google.zxing.Result;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.QRCodeReader;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by maning on 16/7/27.
  * 生成二维码工具
  */
 public class ZXingUtils {
+
+    public static final Map<DecodeHintType, Object> HINTS = new EnumMap<>(DecodeHintType.class);
+
+    static {
+        List<BarcodeFormat> allFormats = new ArrayList<>();
+        allFormats.add(BarcodeFormat.AZTEC);
+        allFormats.add(BarcodeFormat.CODABAR);
+        allFormats.add(BarcodeFormat.CODE_39);
+        allFormats.add(BarcodeFormat.CODE_93);
+        allFormats.add(BarcodeFormat.CODE_128);
+        allFormats.add(BarcodeFormat.DATA_MATRIX);
+        allFormats.add(BarcodeFormat.EAN_8);
+        allFormats.add(BarcodeFormat.EAN_13);
+        allFormats.add(BarcodeFormat.ITF);
+        allFormats.add(BarcodeFormat.MAXICODE);
+        allFormats.add(BarcodeFormat.PDF_417);
+        allFormats.add(BarcodeFormat.QR_CODE);
+        allFormats.add(BarcodeFormat.RSS_14);
+        allFormats.add(BarcodeFormat.RSS_EXPANDED);
+        allFormats.add(BarcodeFormat.UPC_A);
+        allFormats.add(BarcodeFormat.UPC_E);
+        allFormats.add(BarcodeFormat.UPC_EAN_EXTENSION);
+
+        HINTS.put(DecodeHintType.POSSIBLE_FORMATS, allFormats);
+        HINTS.put(DecodeHintType.CHARACTER_SET, "utf-8");
+    }
 
     /**
      * 生成二维码，默认大小为500*500
@@ -141,5 +189,93 @@ public class ZXingUtils {
             return null;
         }
     }
+
+
+    //------解析图片-----代码来自：https://github.com/bingoogolapple/BGAQRCode-Android----感谢
+    /**
+     *
+     * 同步解析本地图片二维码。该方法是耗时操作，请在子线程中调用。
+     *
+     * @param picturePath 要解析的二维码图片本地路径
+     * @return 返回二维码图片里的内容 或 null
+     */
+    public static String syncDecodeQRCode(String picturePath) {
+        return syncDecodeQRCode(getDecodeAbleBitmap(picturePath));
+    }
+
+    /**
+     * 同步解析bitmap二维码。该方法是耗时操作，请在子线程中调用。
+     *
+     * @param bitmap 要解析的二维码图片
+     * @return 返回二维码图片里的内容 或 null
+     */
+    public static String syncDecodeQRCode(Bitmap bitmap) {
+        try {
+            int width = bitmap.getWidth();
+            int height = bitmap.getHeight();
+            int[] pixels = new int[width * height];
+            bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
+            com.google.zxing.RGBLuminanceSource source = new com.google.zxing.RGBLuminanceSource(width, height, pixels);
+            Result result = new MultiFormatReader().decode(new BinaryBitmap(new HybridBinarizer(source)), HINTS);
+            if(result != null){
+                return recode(result.getText());
+            }else{
+                return null;
+            }
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * 将本地图片文件转换成可解码二维码的 Bitmap
+     *
+     * @param picturePath 本地图片文件路径
+     * @return
+     */
+    private static Bitmap getDecodeAbleBitmap(String picturePath) {
+        try {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeFile(picturePath, options);
+            int sampleSize = options.outHeight / 400;
+            if (sampleSize <= 0)
+                sampleSize = 1;
+            options.inSampleSize = sampleSize;
+            options.inJustDecodeBounds = false;
+
+            return BitmapFactory.decodeFile(picturePath, options);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * 中文乱码
+     *
+     * @return
+     */
+    public static String recode(String str) {
+        String formart = "";
+        try {
+            boolean ISO = Charset.forName("ISO-8859-1").newEncoder()
+                    .canEncode(str);
+            if (ISO) {
+                formart = new String(str.getBytes("ISO-8859-1"), "GB2312");
+            } else {
+                formart = str;
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return formart;
+    }
+
+    //---------
+    public static int dip2px(Context context,float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
 
 }

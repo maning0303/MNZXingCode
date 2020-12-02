94175fa765e3a4391c2e391476013198bbc75444
maning <154292322@qq.com>
2020-12-02 16:53:35 +0800

    添加内存泄露检测

---
+++ app/src/main/java/com/maning/zxingcodedemo/MyApplication.java
@@ -0,0 +1,17 @@
+package com.maning.zxingcodedemo;
+
+import android.app.Application;
+
+/**
+ * @author : maning
+ * @date : 2020/12/2
+ * @desc :
+ */
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
